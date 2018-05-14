1ee0d826e34728ad10904a54b62c09437d80ef13
Micha Mazaheri <micha@mazaheri.me>
2018-05-14 23:27:08 +0200

    Custom select

--- src/components/select/index.js
+++ src/components/select/index.js
@@ -3,7 +3,7 @@ import style from './style.scss'
 
 const Select = props => {
   return (
-    <select value={props.value} onChange={props.onChange}>
+    <select className={style.select} value={props.value} onChange={props.onChange}>
       {props.options.map(val => {
         return <option key={val.value} value={val.value}>{val.text}</option>
       })}
