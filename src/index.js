ab8a075b309352f174f47d2e0c1c59aab5708e93
Micha Mazaheri <micha@mazaheri.me>
2018-05-16 16:56:35 +0200

    Remove test with Micha’s name

--- src/index.js
+++ src/index.js
@@ -7,28 +7,6 @@ import Widget from './components/msender'
 
 let _habitat = habitat(Widget);
 
-(() => {
-  let linkElement = document.createElement('link')
-  linkElement.setAttribute('rel', 'stylesheet')
-  linkElement.setAttribute('type', 'text/css')
-  linkElement.setAttribute('href', 'https://fonts.googleapis.com/css?family=Montserrat:900')
-  document.head.appendChild(linkElement)
-})();
-
-setTimeout(() => {
-  jQuery(document).trigger('petition:didSucceed', {
-    postData: {
-      first_name: 'Micha',
-      last_name: 'Mazaheri',
-      email: 'micha@mazaheri.me',
-      city: 'Paris',
-      postal_code: '75015',
-      country_code: 'FR',
-      newsletter: true
-    }
-  })
-}, 1500);
-
 _habitat.render({
   selector: '[data-widget-host="habitat"]',
   clean: true
