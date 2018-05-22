38dc81a17a0bbedae0265fee55ba6c7391153e6b
Micha Mazaheri <micha@mazaheri.me>
2018-05-22 15:55:40 +0200

    Add tests for all departments

--- scripts/departments.py
+++ scripts/departments.py
@@ -13,9 +13,55 @@ d = {
     'region': {
       'name': dep.get('region', {}).get('nom'),
       'code': dep.get('region', {}).get('code'),
-    }
+    },
+    'type': ('drom' if len(dep.get('code')) == 3 else 'metropolitan'),
   } for dep in d
 }
+
+d.update({
+  '975': {
+    'name': 'Saint-Pierre-et-Miquelon',
+    'code': '975',
+    'region': None,
+    'type': 'com',
+  },
+  '977': {
+    'name': 'Saint-Barthélemy',
+    'code': '977',
+    'region': None,
+    'type': 'com',
+  },
+  '978': {
+    'name': 'Saint-Martin',
+    'code': '978',
+    'region': None,
+    'type': 'com',
+  },
+  '986': {
+    'name': 'Wallis-et-Futuna',
+    'code': '986',
+    'region': None,
+    'type': 'com',
+  },
+  '987': {
+    'name': 'Polynésie française',
+    'code': '987',
+    'region': None,
+    'type': 'com',
+  },
+  '988': {
+    'name': 'Nouvelle-Calédonie',
+    'code': '988',
+    'region': None,
+    'type': 'nc',
+  },
+  '999': {
+    'name': 'Français établis hors de France',
+    'code': '999',
+    'region': None,
+    'type': 'foreign',
+  },
+})
 
 with open(os.path.join(dir_path, '../src/models/departements.json'), 'w') as f:
   json.dump(d, f, indent=2, ensure_ascii=False)
--- src/models/department.js
+++ src/models/department.js
@@ -4,10 +4,44 @@ import Region from './region'
 
 import departments from './departements.json'
 
+//
+// Department Types
+//
+
+// Metropolitan Departments (including Corsica)
+export const DEPARTMENT_TYPE_METROPOLITAN = 'metropolitan'
+
+// Overseas department / Département et région ultramarin (DROM)
+export const DEPARTMENT_TYPE_DROM = 'drom'
+
+// Overseas collectivity / Collectivité d'outre-mer (COM)
+export const DEPARTMENT_TYPE_COM = 'com'
+
+// New Caledonia (special status) / Nouvelle-Calédonie
+export const DEPARTMENT_TYPE_NC = 'nc'
+
+// Constituencies for French residents overseas (legislative only)
+// (Circonscriptions législatives des Français établis hors de France)
+export const DEPARTMENT_TYPE_FOREIGN = 'foreign'
+
+//
+// Filter Modes
+//
+
+// Only "real" departments (Metropolitan and DROM)
+export const DEPARTMENT_MODE_DEFAULT = 'default'
+
+// Only metropolitan departments
+export const DEPARTMENT_MODE_METROPOLITAN = 'metropolitan'
+
+// All legislative departments
+export const DEPARTMENT_MODE_LEGISLATIVE = 'legislative'
+
 export default class Department extends Record({
   name: null,
   code: null,
   region: null,
+  type: DEPARTMENT_TYPE_METROPOLITAN,
 }) {
   getDisplayName() {
     return `${this.get('code')} - ${this.get('name')}`
@@ -20,17 +54,34 @@ export default class Department extends Record({
   }
 }
 
-export const getDepartments = () => {
+export const filterDepartmentsFn = (mode = DEPARTMENT_MODE_DEFAULT) => (department) => {
+  const type = department.get('type')
+  switch (mode) {
+    case DEPARTMENT_MODE_DEFAULT:
+      return (type === DEPARTMENT_TYPE_METROPOLITAN ||
+              type === DEPARTMENT_TYPE_DROM)
+    case DEPARTMENT_MODE_METROPOLITAN:
+      return type === DEPARTMENT_TYPE_METROPOLITAN
+    case DEPARTMENT_MODE_LEGISLATIVE:
+      return true
+    default:
+      return false
+  }
+}
+
+export const getDepartments = (mode = DEPARTMENT_MODE_DEFAULT) => {
   return Immutable.Map(departments)
                   .toSeq()
                   .sortBy(d => d.code)
                   .map(d => new Department({
                     name: d.name,
                     code: d.code,
-                    region: new Region({
+                    region: (d.region ? new Region({
                       name: d.region.name,
                       code: d.region.code,
-                    })
+                    }) : null),
+                    type: d.type,
                   }))
+                  .filter(filterDepartmentsFn(mode))
                   .toList()
 }
