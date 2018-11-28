dd6d9a048d3ac4aa80e40c409276f3cdcead9f5c
Micha Mazaheri <micha@mazaheri.me>
2018-11-28 16:35:42 +0100

    Remove React Habitat

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 let poly = require('preact-cli/lib/lib/webpack/polyfills')
 
 import React, { Component } from 'react'
-import habitat from 'preact-habitat'
 
 import MsenderContainer from './components/msender'
-
-// if there's a habitat found, use it
-const habitatSelector = '[data-widget-host="habitat"]'
-if (document.querySelector(habitatSelector)) {
-  const _habitat = habitat(MsenderContainer)
-  _habitat.render({
-    selector: habitatSelector,
-    clean: true,
-  })  
-}
 
 const renderContainer = (domElement, props) => {
   React.render(<MsenderContainer {...props} />, domElement)
